f0c7e943635eeac9f331ddcf029c9e16ab8026e1
Maciej Wilgucki <mwilgucki@gmail.com>
2011-11-24 21:04:53 +0100

    mozliwosc zdefiniowania wlasnego modelu dla kontrolera

--- application/plugins/Universal.php
+++ application/plugins/Universal.php
@@ -8,12 +8,19 @@ class Application_Plugin_Universal extends Zend_Controller_Plugin_Abstract
         $universal = $bootstrap->getOption('universal');
         if(!in_array($controllerName, $universal['page'])) return;
 
+        if(isset($universal['page'][$controllerName]['model'])) {
+            $model = new $universal['page'][$controllerName]['model'];
+        }
+        else {
+            $model = new Batman_Model(strtolower($controllerName));
+        }
+
         $request->setControllerName('universal');
         $request->setParams(
             array(
                 'universal_hash' => $universal['hash'],
                 'original_controller' => $controllerName,
-                'model' => new Batman_Model(strtolower($controllerName))
+                'model' => $model
             )
         );
     }
